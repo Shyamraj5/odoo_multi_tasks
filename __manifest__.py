ee2fb95d8f6260fe65c8ec4393db41a6c19feebc
shyamraj5 <shyamrajkm5@gmail.com>
2024-08-23 09:32:09 +0530

    [ADD]AIO:added quick sale order with invoice creation,delivery,payment posting

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'AIO',
+    'version': '17.0.1.0',
+    # 'summary': 'Automatically generate delivery orders and invoices on sale order confirmation',
+    'author': 'shyamraj',
+    'website': 'https://github.com/Shyamraj5',
+    'category': 'Sales',
+    'depends': ['sale',],
+    'data': [
+             
+             ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ models/sale_order.py
@@ -0,0 +1,32 @@
+from odoo import models, fields
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        for order in self:
+            if order.picking_ids:
+                order.picking_ids.button_validate()
+                order._create_invoices()
+                invoice_line = order.invoice_ids
+                for invoice in invoice_line:
+                    invoice.action_post()
+                    payment_vals = {
+                        'partner_id': order.partner_id.id,
+                        'payment_type': 'inbound',
+                        'amount': invoice.amount_total,
+                        'payment_method_id': invoice.payment_id.payment_method_id.id
+                    }
+                    payment = self.env['account.payment'].create(payment_vals)
+                    payment.action_post()
+
+                payments = [{
+                    'payment': payment,
+                    'to_reconcile': invoice.line_ids,
+                }]
+                payment = self.env['account.payment.register']._reconcile_payments(payments)
+
+        return res
+                    
+# #Finally Done
